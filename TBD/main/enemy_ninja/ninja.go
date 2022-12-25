5446315cbaf9213ed338165c71c075151bf47cdf
saurabhgaglani <saurabhgaglani100@gmail.com>
2022-12-25 22:46:50 +0000

    v4

--- TBD/main/enemy_ninja/ninja.go
+++ TBD/main/enemy_ninja/ninja.go
@@ -1,6 +1,21 @@
 components {
-  id: "arrow"
-  component: "/main/weapon/arrow.script"
+  id: "ninja"
+  component: "/main/enemy_ninja/ninja.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "bloody"
+  component: "/main/enemy_ninja/bloody.particlefx"
   position {
     x: 0.0
     y: 0.0
@@ -16,8 +31,8 @@ components {
 embedded_components {
   id: "sprite"
   type: "sprite"
-  data: "tile_set: \"/main/weapon/weapon.atlas\"\n"
-  "default_animation: \"arrow\"\n"
+  data: "tile_set: \"/main/enemy_ninja/ninja.atlas\"\n"
+  "default_animation: \"ninja_idle\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
   "blend_mode: BLEND_MODE_ALPHA\n"
   ""
@@ -29,8 +44,8 @@ embedded_components {
   rotation {
     x: 0.0
     y: 0.0
-    z: -0.7092184
-    w: 0.70498884
+    z: 0.0
+    w: 1.0
   }
 }
 embedded_components {
@@ -41,14 +56,14 @@ embedded_components {
   "mass: 0.0\n"
   "friction: 0.1\n"
   "restitution: 0.5\n"
-  "group: \"weapon\"\n"
-  "mask: \"enemy\"\n"
+  "group: \"enemy\"\n"
+  "mask: \"weapon\"\n"
   "embedded_collision_shape {\n"
   "  shapes {\n"
   "    shape_type: TYPE_BOX\n"
   "    position {\n"
-  "      x: 0.0\n"
-  "      y: 0.0\n"
+  "      x: -0.168\n"
+  "      y: 0.674\n"
   "      z: 0.0\n"
   "    }\n"
   "    rotation {\n"
@@ -60,9 +75,9 @@ embedded_components {
   "    index: 0\n"
   "    count: 3\n"
   "  }\n"
-  "  data: 9.423\n"
-  "  data: 2.5625\n"
-  "  data: 10.0\n"
+  "  data: 30.5695\n"
+  "  data: 63.104\n"
+  "  data: 9.7\n"
   "}\n"
   "linear_damping: 0.0\n"
   "angular_damping: 0.0\n"
